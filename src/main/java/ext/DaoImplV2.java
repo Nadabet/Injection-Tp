02dd5617274d066f1a2deedab850b9dff3badfab
nadabettach <nadabettach6@gmail.com>
2025-03-02 17:27:56 +0000

    modification

--- src/main/java/ext/DaoImplV2.java
+++ src/main/java/ext/DaoImplV2.java
@@ -4,4 +4,10 @@ import dao.IDao;
 
 public class DaoImplV2 implements IDao {
 
+    @Override
+    public double getData() {
+        System.out.println("Vesion web service");
+        double temp = 11;
+        return temp;
+    }
 }
